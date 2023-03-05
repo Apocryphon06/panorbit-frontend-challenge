167bbe7ee392b343a98b8f840a9906b5230f7968
Apocryphon06 <hrithikchandrashekar@gmail.com>
2023-03-05 13:06:54 +0530

    bg-gradient

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,35 +1,38 @@
 import { Link } from "react-router-dom";
+import bg from '../../assets/images/bg.jpeg'
 
-function Login({usersList}:any) {
-
+function Login({ usersList }: any) {
   return (
-    <div className="lg:w-[570px] w-[325px] m-auto lg:pt-[140px] pt-[100px]">
-      <div className="bg-lightGray rounded-tl-3xl rounded-tr-3xl p-10 shadow-md ">
-        <p className="text-center font-avenirRoman opacity-80 lg:text-xl text-lg">
-          Select an account
-        </p>
-      </div>
-      <div className="bg-white shadow-md rounded-bl-3xl rounded-br-3xl p-2">
-
-        <div className="h-[400px] overflow-y-scroll px-[30px] ">
-        {usersList.map((user: any) => (
-          <>
-            <Link to="/home" state={{ user: user } as any}>
-              <div className="flex flex-row items-center py-[8px] gap-[10px] cursor-pointer border-b">
-                <img
-                  className="lg:w-[32px] w-[28px] lg:h-[32px] h-[28px] rounded-full"
-                  src={user.profilepicture}
-                  alt="profile_pic"
-                />
-                <p className="lg:text-lg text-base font-avenirBook  opacity-70">{user.name}</p>
-              </div>
-            </Link>
-          </>
-        ))}
-
-        <br />
+    <div className="lg:fixed">
+      <img src={bg} alt={bg}/>
+      <div className="lg:w-[570px] w-[325px] absolute lg:top-[130px] top-[50px] lg:left-[450px] left-[125px]">
+        <div className="bg-lightGray rounded-tl-3xl rounded-tr-3xl p-10 shadow-md ">
+          <p className="text-center font-avenir opacity-60 lg:text-xl text-lg">
+            Select an account
+          </p>
         </div>
+        <div className="bg-white shadow-md rounded-bl-3xl rounded-br-3xl p-2">
+          <div className="h-[400px] overflow-y-scroll px-[30px] ">
+            {usersList.map((user: any) => (
+              <>
+                <Link to="/home" state={{ user: user } as any}>
+                  <div className="flex flex-row items-center py-[8px] gap-[10px] cursor-pointer border-b">
+                    <img
+                      className="lg:w-[32px] w-[28px] lg:h-[32px] h-[28px] rounded-full"
+                      src={user.profilepicture}
+                      alt="profile_pic"
+                    />
+                    <p className="lg:text-lg text-base font-avenirBook  opacity-70">
+                      {user.name}
+                    </p>
+                  </div>
+                </Link>
+              </>
+            ))}
 
+            <br />
+          </div>
+        </div>
       </div>
     </div>
   );
